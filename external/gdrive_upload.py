39151711441075071229cd0e061ef2cc5943e6fb
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-03-25 13:45:46 -0300

    improve gdrive_upload.py

--- external/gdrive_upload.py
+++ external/gdrive_upload.py
@@ -1,10 +1,12 @@
 import os
 import lzma
+import gc
 
 import pyrogram
+from pyrogram.errors import UnknownError
 import orjson
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 
 from _config import (
 	PACKAGE_DATA,
@@ -27,35 +29,50 @@ with lzma.open(filename=books_file, mode="r") as file:
 
 # Create GoogleDrive instance with authenticated GoogleAuth instance.
 gauth = GoogleAuth()
+
+print(f"Go to {gauth.GetAuthUrl()}")
+
+gauth.Auth(input("Type the authentication code: "))
 drive = GoogleDrive(gauth)
 
 client = pyrogram.Client(**PYROGRAM_OPTIONS)
 client.start()
 
-for book in books:
+for index, book in enumerate(books):
 	if gdrive.get(book["cover"]["file_unique_id"]) is None:
-		message = client.get_messages(BOOKS_CHAT, book["cover"]["message_id"])
-		upload_file = client.download_media(message)
 		
+		upload_file = None
+		
+		# Baixo a imagem do Telegram e salvo localmente
+		while not upload_file:
+			try:
+				message = client.get_messages(BOOKS_CHAT, book["cover"]["message_id"])
+				upload_file = client.download_media(message)
+			except UnknownError:
+				pass
+		
+		# Envio a imagem local para o Google Drive
 		photo = drive.CreateFile({'title': str(book["title"])})
 		photo.SetContentFile(upload_file)
 		photo.Upload()
 		
-		os.remove(upload_file)
+		# Torno público o arquivo que foi enviado
+		photo.InsertPermission({
+			'type': 'anyone',
+			'value': 'anyone',
+			'role': 'reader'})
 		
-		photo.InsertPermission(
-			{
-				'type': 'anyone',
-				'value': 'anyone',
-				'role': 'reader'
-			}
-		)
-		
+		# Salvo o ID do arquivo enviado
 		gdrive.update({book["cover"]["file_unique_id"]: photo["id"]})
+		
+		# Excluo o arquivo salvo localmente
+		os.remove(upload_file)
 		
 		print(photo['alternateLink'])
 		
 		with lzma.open(filename=gdrive_file, **LZMA_COMPRESSION) as file:
 			file.write(orjson.dumps(gdrive))
+		
+		gc.collect()
 
 client.log_out()
