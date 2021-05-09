4ab8b8f221df4cae031da19f8464b7c2e0454119
SnwMds <46784020+SnwMds@users.noreply.github.com>
2021-05-09 12:10:31 -0300

    update classifiers

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ package_data = {
 classifiers = [
 	"License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
 	"Operating System :: Unix",
+	"Operating System :: Microsoft :: Windows",
+	"Operating System :: MacOS",
 	"Topic :: Internet",
 	"Development Status :: 5 - Production/Stable",
 	"Intended Audience :: Developers",
@@ -23,7 +25,8 @@ classifiers = [
 	"Programming Language :: Python :: 3.7",
 	"Programming Language :: Python :: 3.8",
 	"Programming Language :: Python :: 3.9",
-	"Programming Language :: Python :: 3.10"
+	"Programming Language :: Python :: 3.10",
+	"Programming Language :: Python :: Implementation :: PyPy"
 ]
 
 packages = [
